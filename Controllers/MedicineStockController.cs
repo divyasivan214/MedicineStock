f803f3c56cd3b33448bc1dfb378f65a8cfb2ab6e
Divya Sivan <divyas.d214@gmail.com>
2021-10-20 10:09:22 +0530

    Add project files.

---
+++ Controllers/MedicineStockController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicineStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicineStockController : ControllerBase
+    {
+        private readonly IMedicinestockService imedicineObj;
+        public MedicineStockController(IMedicinestockService msr)
+        {
+            imedicineObj = msr;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetStock()
+        {
+            List<MedicineStock> medStock = await (imedicineObj.GetAllStocks());
+            if (medStock != null)
+            {
+                return Ok(medStock);
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+    }
+}
